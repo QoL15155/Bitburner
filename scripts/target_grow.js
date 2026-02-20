d27f4029294e34a5367cad6b226f2401682b1dff
QoL <www.qol@gmail.com>
2026-02-20 16:02:08 +0200

    hack, grow, weaken scripts enhancement

---
+++ scripts/target_grow.js
@@ -0,0 +1,33 @@
+
+/**
+ * @param {AutocompleteData} data - context about the game, useful when autocompleting
+ * @param {string[]} args - current arguments, not including "run script.js"
+ * @returns {string[]} - the array of possible autocomplete options
+ */
+export function autocomplete(data, args) {
+    const helpOptions = ["-h", "--help"];
+    const defaultOptions = helpOptions.concat("--tail");
+
+    if (args.some(a => helpOptions.includes(a))) {
+        return [];
+    }
+
+    return [...defaultOptions];
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+    const args = ns.flags([['help', false], ['h', false]]);
+    const targetServer = args._[0];
+    if (args.help || args.h || !targetServer) {
+        ns.tprint(`Usage: run ${ns.getScriptName()} TARGET_SERVER`);
+        ns.tprint("");
+        ns.tprint("Grows the server's money.");
+        return;
+    }
+
+    while (true) {
+        await ns.grow(targetServer);
+        await ns.sleep(Math.random() * 100);
+    }
+}
